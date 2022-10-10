26c8fcbd0b746ab72bdebe3606db28c1bbdcd2df
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-10-10 16:06:26 -0700

    Solve problem 6

--- 06_single_letter_count.py
+++ 06_single_letter_count.py
@@ -1,12 +1,24 @@
 def single_letter_count(word, letter):
     """How many times does letter appear in word (case-insensitively)?
-    
+
         >>> single_letter_count('Hello World', 'h')
         1
-        
+
         >>> single_letter_count('Hello World', 'z')
         0
-        
+
         >>> single_letter_count("Hello World", 'l')
         3
     """
+
+    lower_case_word = word.lower()
+    lower_case_letter = letter.lower()
+
+    return len([ltr for ltr in lower_case_word if ltr == lower_case_letter])
+
+    # count = 0
+    # for ltr in lower_case_word:
+    #     if ltr == lower_case_letter:
+    #         count += 1
+
+    # return count
